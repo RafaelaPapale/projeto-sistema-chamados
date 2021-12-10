2984ca263b2d44fd6610b8742093a9000a782a3d
tavares-douglas <douglas.tavares@inatel.br>
2021-12-10 15:48:11 -0300

    :sparkles: na tela de dashboards, atualização das funções de chamado no banco

--- frontEnd/src/pages/Dashboard/index.js
+++ frontEnd/src/pages/Dashboard/index.js
@@ -1,18 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FiEdit, FiMessageSquare, FiPlus, FiSearch } from 'react-icons/fi';
 import { format } from 'date-fns';
 
-import firebase from '../../services/firebaseConnection';
+import { AuthContext } from '../../contexts/auth';
+import ClientChamados from '../../services/chamados';
 import Item from '../../components/Item';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
 
 import './style.css';
 
-const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
-
 export default function Dashboard() {
+    const { user } = useContext(AuthContext);
+
     const [chamados, setChamados] = useState([]);
     const [loading, setLoading] = useState(true);
     const [loadingMore, setLoadingMore] = useState(false);
@@ -23,15 +24,18 @@ export default function Dashboard() {
 
     useEffect(() => {
         async function loadChamados() {
-            await listRef.limit(5)
-                .get()
-                .then((snapshot) => {
-                    updateState(snapshot);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    setLoadingMore(false);
-                })
+            const data = {
+                userId: user.uid,
+            }
+
+            const listChamados = await ClientChamados.listByUser(data);
+
+            if (listChamados.status === 200) {
+                updateState(listChamados);
+            }
+            else {
+                setLoadingMore(false);
+            }
             setLoading(false);
         }
 
@@ -40,28 +44,25 @@ export default function Dashboard() {
         return;
     }, [])
 
-    async function updateState(snapshot) {
-        const isCollectionEmpty = snapshot.size === 0;
+    async function updateState(list) {
+        const isCollectionEmpty = list.data.length === 0;
 
         if (!isCollectionEmpty) {
             let lista = [];
-            snapshot.forEach((doc) => {
+
+            list.data.forEach((doc) => {
                 lista.push({
                     id: doc.id,
-                    assunto: doc.data().assunto,
-                    cliente: doc.data().cliente,
-                    clienteId: doc.data().clienteId,
-                    created: doc.data().created,
-                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
-                    status: doc.data().status,
-                    complemento: doc.data().complemento
+                    assunto: doc.assunto,
+                    cliente: doc.cliente,
+                    clienteId: doc.clienteId,
+                    created: doc.created,
+                    status: doc.status,
+                    complemento: doc.complemento
                 })
             })
 
-            const lastDoc = snapshot.docs[snapshot.docs.length - 1];
-
             setChamados(chamados => [...chamados, ...lista]);
-            setLastDocs(lastDoc);
         }
         else {
             setIsEmpty(true);
@@ -71,15 +72,6 @@ export default function Dashboard() {
 
     async function handleMore() {
         setLoadingMore(true);
-        await listRef.startAfter(lastDocs).limit(5)
-            .get()
-            .then((snapshot) => {
-                updateState(snapshot)
-            })
-            .catch((error) => {
-                console.log(error);
-                //setLoadingMore(false);
-            })
     }
 
     function toggleSearch(item) {
@@ -145,7 +137,7 @@ export default function Dashboard() {
                                             <td data-label="Status">
                                                 <span className="badge" style={{ backgroundColor: item.status === 'Aberto' ? '#5CB85C' : '#999' }}>{item.status}</span>
                                             </td>
-                                            <td data-label="Cadastrado">{item.createdFormated}</td>
+                                            <td data-label="Cadastrado">{item.created}</td>
                                             <td data-label="#">
                                                 <button className="action" style={{ backgroundColor: "#3583F6" }} onClick={() => toggleSearch(item)}>
                                                     <FiSearch color="white" size={17} />
@@ -168,7 +160,7 @@ export default function Dashboard() {
             </div>
 
             {showItem && (
-                <Item conteudo={detail} close={toggleSearch}/>
+                <Item conteudo={detail} close={toggleSearch} />
             )}
         </div>
     );
