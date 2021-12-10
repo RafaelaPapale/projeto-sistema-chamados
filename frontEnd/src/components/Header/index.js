1af639262d0ffa1707188d7cc563179750fe66d1
tavares-douglas <douglas.tavares@inatel.br>
2021-12-10 15:43:04 -0300

    :sparkles: criação do elemento header no front

--- frontEnd/src/components/Header/index.js
+++ frontEnd/src/components/Header/index.js
@@ -1,5 +1,3 @@
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/auth';
 import { Link } from 'react-router-dom';
 import { FiHome, FiUser, FiSettings } from "react-icons/fi";
 
@@ -7,11 +5,10 @@ import avatar from '../../assets/avatar.png';
 import './style.css';
 
 export default function Header() {
-    const { user } = useContext(AuthContext);
     return (
         <div className="sidebar">
             <div>
-                <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto de perfil do usuário" />
+                <img src={avatar} alt="Foto de perfil do usuário" />
             </div>
             <Link to="/dashboard">
                 <FiHome color="white" size={24} />
