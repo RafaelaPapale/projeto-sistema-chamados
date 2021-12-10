3e25669bf3b490b35c01856700b5aefc2f8d772a
tavares-douglas <douglas.tavares@inatel.br>
2021-12-10 15:49:27 -0300

    :sparkles: na tela de profile, atualização das funções de chamado no banco

--- frontEnd/src/pages/Profile/index.js
+++ frontEnd/src/pages/Profile/index.js
@@ -1,9 +1,9 @@
 import { FiSettings, FiUpload } from 'react-icons/fi';
 import { AuthContext } from '../../contexts/auth';
 import { useContext, useState } from 'react';
-//import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
-import firebase from '../../services/firebaseConnection';
+import ClientUsers from '../../services/user';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
@@ -16,81 +16,24 @@ export default function Profile() {
 
     const [nome, setNome] = useState(user && user.nome);
     const [email, setEmail] = useState(user && user.email);
-    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
-
-    const [imageAvatar, setImageAvatar] = useState(null);
-
-    function handleFile(e) {
-        if (e.target.files[0]) {
-            const image = e.target.files[0];
-
-            if (image.type === 'image/jpeg' || image.type === 'image/png') {
-                setImageAvatar(image);
-                setAvatarUrl(URL.createObjectURL(image));
-            } else {
-                alert('Envie uma imagem do tipo .PNG ou .JPEG');
-                setImageAvatar(null);
-                return null;
-            }
-        }
-    }
-
-    async function handleUpload() {
-        const currentUid = user.uid;
-        const uploadTask = await firebase.storage()
-            .ref(`images/${currentUid}/${imageAvatar.name}`)
-            .put(imageAvatar)
-            .then(async () => {
-                console.log("Foto enviada com sucesso")
-
-                await firebase.storage().ref(`images/${currentUid}`)
-                    .child(imageAvatar.name).getDownloadURL()
-                    .then(async (url) => {
-                        let urlFoto = url;
-                        await firebase.firestore().collection('users')
-                            .doc(user.uid)
-                            .update({
-                                avatarUrl: urlFoto,
-                                nome: nome
-                            })
-                            .then(() => {
-                                let data = {
-                                    ...user,
-                                    avatarUrl: urlFoto,
-                                    nome: nome
-                                };
-                                setUser(data);
-                                storageUser(data);
-                            })
-
-                    })
-
-            })
-    }
 
     async function handleSave(e) {
         e.preventDefault();
-        if (imageAvatar === null & nome !== '') {
-            await firebase.firestore().collection('users')
-                .doc(user.uid)
-                .update({
-                    nome: nome
-                })
-                .then(() => {
-                    let data = {
-                        ...user,
-                        nome: nome
-                    };
-                    setUser(data);
-                    storageUser(data);
-                })
-                .catch((error) => {
-                    alert(`${error}`)
-                })
+        
+        const data = {
+            nome: nome,
+            email: email,
+            id: user.uid,
         }
-        else if (nome !== '' && imageAvatar !== null) {
-            //alert('CAIU AQUI')
-            handleUpload();
+
+        const update = await ClientUsers.updateUser(data);
+        console.log(update);
+        if (update.status === 200) {
+            setUser(update.data);
+            storageUser(update.data);
+            toast.success('Usuário atualizado com sucesso!');
+        } else {
+            toast.error('Ops algo deu errado!');
         }
     }
 
@@ -105,20 +48,7 @@ export default function Profile() {
 
                 <div className="container">
                     <form className="form-profile" onSubmit={handleSave}>
-                        <label className="label-avatar">
-                            <span>
-                                <FiUpload color="white" size={25} />
-                            </span>
-
-                            <input type="file" accept="image/" onChange={handleFile} />
-                            <br />
-                            {avatarUrl === null ?
-                                <img src={avatar} width="250" height="250" alt="Foto de perfil do usuário" />
-                                :
-                                <img src={avatarUrl} width="250" height="250" alt="Foto de perfil do usuário" />
-                            }
-                        </label>
-
+                        
                         <label>Nome</label>
                         <input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
 
