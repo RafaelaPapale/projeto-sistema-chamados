97b1232471b97e1a9000a28ed670031fe6c33cd6
Rafaela de Moraes Papale <rafaela.papale@gec.inatel.br>
2021-12-10 07:51:59 -0300

    :recycle: Corrigir erros do frontend

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,25 +1,15 @@
 import 'react-toastify/dist/ReactToastify.css';
-
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AuthProvider from './contexts/auth';
-
 import Routes from './routes';
-
-const mdTheme = createTheme();
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <ThemeProvider theme={mdTheme}>
-          <CssBaseline />
-          <ToastContainer autoClose={3000} />
-          <Routes />
-        </ThemeProvider>
+        <ToastContainer autoClose={3000} />
+        <Routes />
       </BrowserRouter>
     </AuthProvider>
   );
--- frontEnd/src/components/Header/index.js
+++ frontEnd/src/components/Header/index.js
@@ -1,101 +1,30 @@
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import MuiDrawer from '@mui/material/Drawer';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Toolbar from '@mui/material/Toolbar';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleIcon from '@mui/icons-material/People';
-import LayersIcon from '@mui/icons-material/Layers';
-import List from '@mui/material/List';
-
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { AuthContext } from '../../contexts/auth';
 import { Link } from 'react-router-dom';
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
 
+import avatar from '../../assets/avatar.png';
 import './style.css';
 
-const drawerWidth = 240;
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-        '& .MuiDrawer-paper': {
-            position: 'relative',
-            whiteSpace: 'nowrap',
-            width: drawerWidth,
-            transition: theme.transitions.create('width', {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            boxSizing: 'border-box',
-            ...(!open && {
-                overflowX: 'hidden',
-                transition: theme.transitions.create('width', {
-                    easing: theme.transitions.easing.sharp,
-                    duration: theme.transitions.duration.leavingScreen,
-                }),
-                width: theme.spacing(7),
-                [theme.breakpoints.up('sm')]: {
-                    width: theme.spacing(9),
-                },
-            }),
-        },
-    }),
-);
-
 export default function Header() {
-    //const { user } = useContext(AuthContext);
-    const [open, setOpen] = useState(true);
-    const toggleDrawer = () => {
-        setOpen(!open);
-    };
+    const { user } = useContext(AuthContext);
     return (
-        <Box>
-            <Drawer variant="permanent" open={open}>
-                <Toolbar className="toolbar">
-                    <IconButton onClick={toggleDrawer}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </Toolbar>
-                <Divider />
-                <List>
-                    <Link to="/dashboard">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DashboardIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Dashboard" />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Divider />
-                <List>
-                    <Link to="/customers">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <PeopleIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Customers" />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Divider />
-                <List>
-                    <Link to="/profile">
-                        <ListItem button>
-                            <ListItemIcon>
-                                <LayersIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Profile" />
-                        </ListItem>
-                    </Link>
-                </List>
-                <Divider />
-            </Drawer>
-        </Box>
+        <div className="sidebar">
+            <div>
+                <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto de perfil do usuário" />
+            </div>
+            <Link to="/dashboard">
+                <FiHome color="white" size={24} />
+                Chamados
+            </Link>
+            <Link to="/customers">
+                <FiUser color="white" size={24} />
+                Clientes
+            </Link>
+            <Link to="/profile">
+                <FiSettings color="white" size={24} />
+                Configurações
+            </Link>
+        </div>
     )
 }
