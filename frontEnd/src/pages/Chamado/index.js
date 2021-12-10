6a4ec14e26ee2416b23359d6c5eadd3012a66028
tavares-douglas <douglas.tavares@inatel.br>
2021-12-10 15:46:14 -0300

    :sparkles: Atualizações das funções de chamado no banco

--- frontEnd/src/pages/Chamado/index.js
+++ frontEnd/src/pages/Chamado/index.js
@@ -4,7 +4,8 @@ import { AuthContext } from '../../contexts/auth';
 import { toast } from 'react-toastify';
 import { useHistory, useParams } from 'react-router-dom';
 
-import firebase from '../../services/firebaseConnection';
+import ClientCustomers from '../../services/customers';
+import ClientChamados from '../../services/chamados';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
 
@@ -28,15 +29,15 @@ export default function Chamado() {
 
     useEffect(() => {
         async function loadCustomers() {
-            await firebase.firestore().collection('customers')
-                .get()
-                .then((snapshot) => {
-                    let lista = [];
-                    snapshot.forEach((doc) => {
-                        lista.push({
-                            id: doc.id,
-                            nomeFantasia: doc.data().nomeFantasia
-                        })
+            const listCustomers = await ClientCustomers.listCustomers();
+
+            if (listCustomers.status === 200) {
+                let lista = [];
+
+                listCustomers.data.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        nomeFantasia: doc.nomeFantasia
                     })
 
                     if (lista.length === 0) {
@@ -49,89 +50,90 @@ export default function Chamado() {
                     setCustomers(lista);
                     setLoadCustomers(false);
 
-                    if(id){
+                    if (id) {
                         loadId(lista);
                     }
                 })
-                .catch((error) => {
-                    console.log(error);
-                    setLoadCustomers(false);
-                    setCustomers([{ id: 1, nomeFantasia: '' }])
-                })
+            }
+            else {
+                setLoadCustomers(false);
+                setCustomers([{ id: 1, nomeFantasia: '' }])
+            }
+        }
+
+        async function loadId(lista) {
+            const data = {
+                id: id,
+            };
+
+            const findChamados = await ClientChamados.listById(data);
+
+            if (findChamados.status === 200) {
+                setAssunto(findChamados.data.assunto);
+                setStatus(findChamados.data.status);
+                setComplemento(findChamados.data.complemento);
+
+                let index = lista.findIndex(item => item.id === findChamados.data.clienteId);
+
+                setCustomerSelected(index);
+                setIdCustomer(true);
+            }
+            else {
+                toast.error("Esse chamado não existe! Tente novamente!");
+                history.push('/dashboard');
+                setIdCustomer(false);
+            }
         }
 
         loadCustomers();
-    }, [id, loadId]);
-
-    async function loadId(lista){
-        await firebase.firestore().collection('chamados')
-        .doc(id)
-        .get()
-        .then((snapshot) => {
-            //console.log(`${snapshot.data().assunto}`)
-            setAssunto(snapshot.data().assunto);
-            setStatus(snapshot.data().status);
-            setComplemento(snapshot.data().complemento);
-
-            let index = lista.findIndex(item => item.id === snapshot.data().clienteId);
-
-            setCustomerSelected(index);
-            setIdCustomer(true);
-        })
-        .catch((error) => {
-            console.log(error);
-            toast.error("Esse chamado não existe! Tente novamente!");
-            history.push('/dashboard');
-            setIdCustomer(false);
-        })
-    }
+    }, [id]);
 
     async function handleRegister(e) {
         e.preventDefault();
-
-        if(idCustomer){
-            await firebase.firestore().collection('chamados')
-            .doc(id)
-            .update({
+        if (idCustomer) {
+            const data = {
                 cliente: customers[customerSelected].nomeFantasia,
-                clienteId: customers[customerSelected].id,
+                clientId: customers[customerSelected].id,
                 assunto: assunto,
                 status: status,
                 complemento: complemento,
-                userId: user.uid
-            })
-            .then(() => {
+                userId: user.uid,
+            };
+
+            const updateChamado = await ClientChamados.updateChamado(data);
+
+            if (updateChamado.status === 200) {
                 setComplemento('');
                 setCustomerSelected(0);
                 history.push('/dashboard');
                 toast.success("Chamado editado com sucesso!");
-            })
-            .catch((error) => {
-                console.log(error);
+            }
+            else {
                 toast.error("Ops! Erro ao editar chamado!");
-            })
+            }
             return;
         }
 
-        await firebase.firestore().collection('chamados')
-            .add({
-                created: new Date(),
-                cliente: customers[customerSelected].nomeFantasia,
-                clienteId: customers[customerSelected].id,
-                assunto: assunto,
-                status: status,
-                complemento: complemento,
-                userId: user.uid
-            })
-            .then(() => {
-                toast.success("Chamado registrado com sucesso!");
+        const data = {
+            created: JSON.stringify(new Date()),
+            cliente: customers[customerSelected].nomeFantasia,
+            clientId: customers[customerSelected].id,
+            assunto: assunto,
+            status: status,
+            complemento: complemento,
+            userId: user.uid,
+        };
+
+        const createChamado = await ClientChamados.createChamado(data);
+
+        if(createChamado.status === 200){
+            toast.success("Chamado registrado com sucesso!");
                 setComplemento('');
                 setCustomerSelected(0);
-            })
-            .catch((error) => {
-                console.log(error);
-                toast.error("Ops! Erro ao registrar chamado!");
-            })
+        }
+        else {
+            toast.error("Ops! Erro ao registrar chamado!");
+        }
     }
 
     function handleChangeSelect(e) {
